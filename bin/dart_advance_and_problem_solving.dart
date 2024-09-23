677ba1343d43df801dc7932277ee12dd4676d79f
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-23 06:05:09 +0600

    update

---
+++ bin/dart_advance_and_problem_solving.dart
@@ -0,0 +1,5 @@
+import 'package:dart_advance_and_problem_solving/dart_advance_and_problem_solving.dart' as dart_advance_and_problem_solving;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_advance_and_problem_solving.calculate()}!');
+}
---
+++ test/dart_advance_and_problem_solving_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_advance_and_problem_solving/dart_advance_and_problem_solving.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
